565f5b6e7bb742c9baff96b82536dd8aafbede95
rrymar <rrymar@outlook.com>
2021-02-12 16:42:00 +0200

    931. Minimum Falling Path Sum init

---
+++ Matrix.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace LeetCode
+{
+    public static class Matrix
+    {
+        public static int[][] Parse(string matrixString)
+        {
+            var trimmed = matrixString.Substring(1, matrixString.Length - 2);
+            var rows = trimmed.Split("],[").Select(e => e.Trim('[', ']'));
+
+            return rows.Select(e => e.Split(',').Select(int.Parse).ToArray())
+                .ToArray();
+        }
+    }
+
+    public class MatrixTests
+    {
+        [Fact]
+        public void ItShouldParseMultiRowMatrix()
+        {
+            var result = Matrix.Parse("[[2,1,3],[6,5,4],[7,8,9]]");
+            result[0][0].Should().Be(2);
+            result[1][1].Should().Be(5);
+            result[2][2].Should().Be(9);
+        }
+
+        [Fact]
+        public void ItShouldParseSingleRowMatrix()
+        {
+            var result = Matrix.Parse("[[2,1,3]]");
+            result[0][0].Should().Be(2);
+            result[0][1].Should().Be(1);
+            result[0][2].Should().Be(3);
+        }
+    }
+}
---
+++ MinFallingPathSum/MinFallingPathSumAlgorithm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.MinFallingPathSum
+{
+    public class MinFallingPathSumAlgorithm
+    {
+        public class Node
+        {
+            public Node(int value, int x)
+            {
+                Value = value;
+                X = x;
+            }
+            public int Value { get; }
+            public int X { get; }
+        }
+
+        public int MinFallingPathSum(int[][] matrix)
+        {
+            if (matrix.Length == 1) return matrix[0].Min();
+
+            var min = int.MaxValue;
+
+            var xSize = matrix[0].Length;
+            for (int x = 0; x < xSize; x++)
+            {
+                var nodes = new List<Node> {new Node(matrix[0][x], x)};
+
+                for (int y = 1; y < matrix.Length; y++)
+                {
+                    var nextLevelNodes = new List<Node>(y * 3);
+                    foreach (var node in nodes)
+                    {
+                        if (node.X > 0)
+                            nextLevelNodes.Add(new Node(node.Value + matrix[y][node.X - 1], node.X - 1));
+                        if(node.X+1 < xSize)
+                            nextLevelNodes.Add(new Node(node.Value + matrix[y][node.X + 1], node.X + 1));
+
+                        nextLevelNodes.Add(new Node(node.Value + matrix[y][node.X], node.X));
+                    }
+
+                    nodes = nextLevelNodes;
+                }
+
+                var currentMin = nodes.Min(e => e.Value);
+
+                if (currentMin < min) min = currentMin;
+            }
+
+            return min;
+        }
+
+        private int CalculateSimplePathSum(int[][] matrix)
+        {
+            var min = int.MaxValue;
+            for (int x = 0; x < matrix[0].Length; x++)
+            {
+                var currentMin = 0;
+                for (int y = 0; y < matrix.Length; y++)
+                {
+                    currentMin += matrix[y][x];
+                    if (currentMin > min) break;
+                }
+
+                if (currentMin < min) min = currentMin;
+            }
+
+            return min;
+        }
+    }
+}
