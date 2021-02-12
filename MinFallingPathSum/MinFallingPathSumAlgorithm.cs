79ab21479c14119a1f570e2fd1c5f845207af926
rrymar <rrymar@outlook.com>
2021-02-12 17:24:51 +0200

    931. Minimum Falling Path Sum optimized

--- MinFallingPathSum/MinFallingPathSumAlgorithm.cs
+++ MinFallingPathSum/MinFallingPathSumAlgorithm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,12 +9,13 @@ namespace LeetCode.MinFallingPathSum
     {
         public class Node
         {
-            public Node(int value, int x)
+            public Node(int x, int value)
             {
-                Value = value;
                 X = x;
+                Values.Add(value);
             }
-            public int Value { get; }
+
+            public readonly List<int> Values = new List<int>();
             public int X { get; }
         }
 
@@ -26,25 +28,50 @@ namespace LeetCode.MinFallingPathSum
             var xSize = matrix[0].Length;
             for (int x = 0; x < xSize; x++)
             {
-                var nodes = new List<Node> {new Node(matrix[0][x], x)};
+                var nodes = new Dictionary<int, List<int>>();
+                nodes.Add(x, new List<int>(new[] {matrix[0][x]}));
 
                 for (int y = 1; y < matrix.Length; y++)
                 {
-                    var nextLevelNodes = new List<Node>(y * 3);
-                    foreach (var node in nodes)
+                    var newNodes = new Dictionary<int, List<int>>();
+                    foreach (var nodeX in nodes.Keys.ToList())
                     {
-                        if (node.X > 0)
-                            nextLevelNodes.Add(new Node(node.Value + matrix[y][node.X - 1], node.X - 1));
-                        if(node.X+1 < xSize)
-                            nextLevelNodes.Add(new Node(node.Value + matrix[y][node.X + 1], node.X + 1));
+                        var prevRowValue = nodes[nodeX];
 
-                        nextLevelNodes.Add(new Node(node.Value + matrix[y][node.X], node.X));
+                        var current = matrix[y][nodeX];
+                        var value = prevRowValue.Select(e => e + current).ToList();
+                        if (!newNodes.ContainsKey(nodeX))
+                            newNodes.Add(nodeX, value);
+                        else
+                            newNodes[nodeX].AddRange(value);
+
+                        var nextX = nodeX + 1;
+                        if (nextX < xSize)
+                        {
+                            current = matrix[y][nextX];
+                            var nextValue = prevRowValue.Select(e => e + current).ToList();
+                            if (!newNodes.ContainsKey(nextX))
+                                newNodes.Add(nextX, nextValue);
+                            else
+                                newNodes[nextX].AddRange(nextValue);
+                        }
+
+                        var prevX = nodeX - 1;
+                        if (prevX >= 0)
+                        {
+                            current = matrix[y][prevX];
+                            var prevValue = prevRowValue.Select(e => e + current).ToList();
+                            if (!newNodes.ContainsKey(prevX))
+                                newNodes.Add(prevX, prevValue);
+                            else
+                                newNodes[prevX].AddRange(prevValue);
+                        }
                     }
 
-                    nodes = nextLevelNodes;
+                    nodes = newNodes;
                 }
 
-                var currentMin = nodes.Min(e => e.Value);
+                var currentMin = nodes.SelectMany(e=>e.Value).Min(e => e);
 
                 if (currentMin < min) min = currentMin;
             }
