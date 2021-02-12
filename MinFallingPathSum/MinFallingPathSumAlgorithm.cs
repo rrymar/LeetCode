d73746782d5dd5ada18fa1bb59c10e1e8ac3e67d
rrymar <rrymar@outlook.com>
2021-02-12 17:31:49 +0200

    931. Minimum Falling Path Sum optimized 2

--- MinFallingPathSum/MinFallingPathSumAlgorithm.cs
+++ MinFallingPathSum/MinFallingPathSumAlgorithm.cs
@@ -7,18 +7,6 @@ namespace LeetCode.MinFallingPathSum
 {
     public class MinFallingPathSumAlgorithm
     {
-        public class Node
-        {
-            public Node(int x, int value)
-            {
-                X = x;
-                Values.Add(value);
-            }
-
-            public readonly List<int> Values = new List<int>();
-            public int X { get; }
-        }
-
         public int MinFallingPathSum(int[][] matrix)
         {
             if (matrix.Length == 1) return matrix[0].Min();
@@ -28,43 +16,43 @@ namespace LeetCode.MinFallingPathSum
             var xSize = matrix[0].Length;
             for (int x = 0; x < xSize; x++)
             {
-                var nodes = new Dictionary<int, List<int>>();
-                nodes.Add(x, new List<int>(new[] {matrix[0][x]}));
+                var nodes = new Dictionary<int, HashSet<int>>();
+                nodes.Add(x, new HashSet<int>(new[] {matrix[0][x]}));
 
                 for (int y = 1; y < matrix.Length; y++)
                 {
-                    var newNodes = new Dictionary<int, List<int>>();
+                    var newNodes = new Dictionary<int, HashSet<int>>();
                     foreach (var nodeX in nodes.Keys.ToList())
                     {
                         var prevRowValue = nodes[nodeX];
 
                         var current = matrix[y][nodeX];
-                        var value = prevRowValue.Select(e => e + current).ToList();
+                        var value = prevRowValue.Select(e => e + current).ToHashSet();
                         if (!newNodes.ContainsKey(nodeX))
                             newNodes.Add(nodeX, value);
                         else
-                            newNodes[nodeX].AddRange(value);
+                            newNodes[nodeX].UnionWith(value);
 
                         var nextX = nodeX + 1;
                         if (nextX < xSize)
                         {
                             current = matrix[y][nextX];
-                            var nextValue = prevRowValue.Select(e => e + current).ToList();
+                            var nextValue = prevRowValue.Select(e => e + current).ToHashSet();
                             if (!newNodes.ContainsKey(nextX))
                                 newNodes.Add(nextX, nextValue);
                             else
-                                newNodes[nextX].AddRange(nextValue);
+                                newNodes[nextX].UnionWith(nextValue);
                         }
 
                         var prevX = nodeX - 1;
                         if (prevX >= 0)
                         {
                             current = matrix[y][prevX];
-                            var prevValue = prevRowValue.Select(e => e + current).ToList();
+                            var prevValue = prevRowValue.Select(e => e + current).ToHashSet();
                             if (!newNodes.ContainsKey(prevX))
                                 newNodes.Add(prevX, prevValue);
                             else
-                                newNodes[prevX].AddRange(prevValue);
+                                newNodes[prevX].UnionWith(prevValue);
                         }
                     }
 
@@ -72,24 +60,6 @@ namespace LeetCode.MinFallingPathSum
                 }
 
                 var currentMin = nodes.SelectMany(e=>e.Value).Min(e => e);
-
-                if (currentMin < min) min = currentMin;
-            }
-
-            return min;
-        }
-
-        private int CalculateSimplePathSum(int[][] matrix)
-        {
-            var min = int.MaxValue;
-            for (int x = 0; x < matrix[0].Length; x++)
-            {
-                var currentMin = 0;
-                for (int y = 0; y < matrix.Length; y++)
-                {
-                    currentMin += matrix[y][x];
-                    if (currentMin > min) break;
-                }
 
                 if (currentMin < min) min = currentMin;
             }
