a156ee84cea1b75a566ec744b8fd2310cb35031d
rrymar <rrymar@outlook.com>
2021-02-15 17:10:34 +0200

    https://leetcode.com/problems/longest-substring-without-repeating-characters/

---
+++ LongestSubstring/LongestSubstringAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.LongestSubstring
+{
+    /// <summary>
+    /// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    /// </summary>
+    public class LongestSubstringAlgorithm
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var map = new ushort[128];
+            ushort result = 0, start = 0;
+            for (ushort end = 0; end < s.Length; end++)
+            {
+                var currentChar = s[end];
+
+                start = map[currentChar] > start ? map[currentChar] : start;
+
+                var newResult = (ushort) (end - start + 1);
+                if (newResult > result)
+                    result = newResult;
+
+                map[currentChar] = (ushort) (end + 1);
+            }
+
+
+            return result;
+        }
+    }
+}
---
+++ LongestSubstring/LongestSubstringTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.LongestSubstring
+{
+    public class LongestSubstringTests
+    {
+        private readonly LongestSubstringAlgorithm algorithm = new();
+
+        [Theory]
+        [InlineData("abcabcbb",3)]
+        [InlineData("bbbbb",1)]
+        [InlineData("pwwkew",3)]
+        [InlineData("",0)]
+        public void IsShouldReturnSubstringLength(string str, int expected)
+        {
+            algorithm.LengthOfLongestSubstring(str).Should().Be(expected);
+        }
+
+    }
+}
