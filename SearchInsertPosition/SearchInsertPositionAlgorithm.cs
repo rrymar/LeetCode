e2f2d7bc6548a2d6e2aa2210c03e51c71540450e
rrymar <rrymar@outlook.com>
2021-02-15 18:07:46 +0200

    https://leetcode.com/problems/search-insert-position/ optimized

--- SearchInsertPosition/SearchInsertPositionAlgorithm.cs
+++ SearchInsertPosition/SearchInsertPositionAlgorithm.cs
@@ -8,27 +8,22 @@
         public int SearchInsert(int[] nums, int target)
         {
             if (nums.Length == 0) return 0;
-            if (nums.Length == 1) return nums[0] >= target ? 0 : 1;
 
             var min = 0;
             var max = nums.Length - 1;
 
-            while (true)
+            while (min <= max)
             {
                 var middle = (max - min + 1) / 2 + min;
                 if (nums[middle] == target) return middle;
 
                 if (nums[middle] > target)
-                {
-                    if (min == max) return middle == 0 ? 0 : middle - 1;
-                    max = middle == max ? min : middle;
-                }
+                    max =  middle - 1;
                 else
-                {
-                    if (min == max) return middle + 1;
-                    min = middle == min ? max : middle;
-                }
+                    min = middle + 1;
             }
+
+            return min;
         }
     }
 }
