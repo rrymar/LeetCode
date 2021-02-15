791cab3c5784a3311fab1e7b71505b17427e61bf
rrymar <rrymar@outlook.com>
2021-02-15 17:59:10 +0200

    https://leetcode.com/problems/search-insert-position/

---
+++ SearchInsertPosition/SearchInsertPositionAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.SearchInsertPosition
+{
+    /// <summary>
+    /// https://leetcode.com/problems/search-insert-position/
+    /// </summary>
+    public class SearchInsertPositionAlgorithm
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0] >= target ? 0 : 1;
+
+            var min = 0;
+            var max = nums.Length - 1;
+
+            while (true)
+            {
+                var middle = (max - min + 1) / 2 + min;
+                if (nums[middle] == target) return middle;
+
+                if (nums[middle] > target)
+                {
+                    if (min == max) return middle == 0 ? 0 : middle - 1;
+                    max = middle == max ? min : middle;
+                }
+                else
+                {
+                    if (min == max) return middle + 1;
+                    min = middle == min ? max : middle;
+                }
+            }
+        }
+    }
+}
---
+++ SearchInsertPosition/SearchInsertPositionTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.SearchInsertPosition
+{
+    public class SearchInsertPositionTests
+    {
+        private readonly SearchInsertPositionAlgorithm algorithm = new();
+
+        [Theory]
+        [InlineData("[1,3,5,6]", 5, 2)]
+        [InlineData("[1,3,5,6]", 2, 1)]
+        [InlineData("[1,3,5,6,11]", 2, 1)]
+        [InlineData("[1,3,5,6]", 7, 4)]
+        [InlineData("[1,3,5,6]", 0, 0)]
+        [InlineData("[1]", 0, 0)]
+        [InlineData("[1]", 1, 0)]
+        [InlineData("[1,3]", 1, 0)]
+        public void IsShouldReturnInsertPosition(string arrayStr, int number, int expected)
+        {
+            var array = IntArray.Parse(arrayStr);
+            algorithm.SearchInsert(array, number).Should().Be(expected);
+        }
+    }
+}
