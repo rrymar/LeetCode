646001ac8fba6f3f6fb7cc8874403973e3aee303
Ruslan <ruslan.rimar@gmail.com>
2021-03-22 14:35:10 +0200

    https://leetcode.com/problems/three-consecutive-odds/

---
+++ ThreeConsecutiveOdds/ThreeConsecutiveOddsAlgorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.ThreeConsecutiveOdds
+{
+    /// <summary>
+    /// https://leetcode.com/problems/three-consecutive-odds/
+    /// </summary>
+    public class ThreeConsecutiveOddsAlgorithm
+    {
+        public bool ThreeConsecutiveOdds(int[] arr)
+        {
+            var sequenceLen = 0;
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    sequenceLen++;
+                    if (sequenceLen == 3) return true;
+                }
+                else
+                {
+                    sequenceLen = 0;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ThreeConsecutiveOdds/ThreeConsecutiveOddsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.ThreeConsecutiveOdds
+{
+    public class ThreeConsecutiveOddsTests
+    {
+        private readonly ThreeConsecutiveOddsAlgorithm algorithm = new();
+
+        [Theory]
+        [InlineData(false,2,6,4,1)]
+        [InlineData(true,1,2,34,3,4,5,7,23,12)]
+        public void IsShouldReturnInteger(bool expected, params int[] numbers)
+        {
+            algorithm.ThreeConsecutiveOdds(numbers).Should().Be(expected);
+        }
+    }
+}
