3960b36392719825f245cbae5250281adb591d02
RuslanRymar <rrimar@idiq.com>
2022-07-26 17:43:21 +0300

    https://leetcode.com/problems/container-with-most-wate

---
+++ ContainerWithMostWater/ContainerWithMostWaterAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.ContainerWithMostWater
+{
+    /// <summary>
+    /// hhttps://leetcode.com/problems/container-with-most-water/
+    /// </summary>
+    public class ContainerWithMostWaterAlgorithm
+    {
+        public int MaxArea(int[] height)
+        {
+            var max = 0;
+            var left = 0;
+            var right = height.Length - 1;
+            while(left < right)
+            {
+                var waterAmount = (right - left) * Math.Min(height[right], height[left]);
+                if (waterAmount > max) max = waterAmount;
+                if (height[left] <= height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ ContainerWithMostWater/ContainerWithMostWaterTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.ContainerWithMostWater
+{
+    public class ContainerWithMostWaterTests
+    {
+        private readonly ContainerWithMostWaterAlgorithm algorithm = new();
+
+        [Theory]
+        [InlineData("[1,8,6,2,5,4,8,3,7]",49)]
+        [InlineData("[1,1]",1)]
+        public void IsShouldReturnSubstringLength(string arrayString, int expected)
+        {
+            algorithm.MaxArea(IntArray.Parse(arrayString)).Should().Be(expected);
+        }
+
+    }
+}
