9abfddbf181d29a0d5cd45d5faf882f41d53bc5e
rrymar <rrymar@outlook.com>
2025-03-01 18:47:20 +0200

    https://leetcode.com/problems/merge-sorted-array

---
+++ MergeSortedArray/MergeSortedArraysAlgorithm.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.MergeSortedArray
+{
+    /// <summary>
+    /// https://leetcode.com/problems/merge-sorted-array/
+    /// </summary>
+    public class MergeSortedArraysAlgorithm
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (n == 0) return;
+
+            var output = m != 0 ? new int[m + n] : nums2;
+
+            var index1 = 0;
+            var index2 = 0;
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (index2 >= n || (index1 < m && nums1[index1] <= nums2[index2]))
+                {
+                    output[i] = nums1[index1];
+                    index1++;
+                }
+                else
+                {
+                    output[i] = nums2[index2];
+                    index2++;
+                }
+            }
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                nums1[i] = output[i];
+            }
+        }
+
+        public void Merge2(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (n == 0) return;
+
+            var output = new List<int>(m + n);
+
+            var index1 = 0;
+            var index2 = 0;
+
+            var firstDone = false;
+            var secondDone = false;
+
+            while (!firstDone || !secondDone)
+            {
+                if (!firstDone && nums1[index1] <= nums2[index2])
+                {
+                    output.Add(nums1[index1]);
+                    index1++;
+                }
+                else
+                {
+                    output.Add(nums2[index2]);
+                    index2++;
+                }
+
+                firstDone = index1 >= m;
+                secondDone = index2 >= n;
+            }
+
+
+            for (int i = 0; i < output.Count; i++)
+            {
+                nums1[i] = output[i];
+            }
+        }
+    }
+}
---
+++ MergeSortedArray/MergeSortedArraysTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using LeetCode.RemoveElement;
+using Xunit;
+
+namespace LeetCode.MergeSortedArray
+{
+    public class MergeSortedArraysTests
+    {
+        private readonly MergeSortedArraysAlgorithm algorithm = new();
+
+        [Fact]
+        public void ItMergesArrays()
+        {
+            var nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int n = 3;
+            var nums2 = new[] { 2, 5, 6 };
+            algorithm.Merge(nums1, m, nums2, n);
+
+            nums1.Should().BeEquivalentTo(new[] { 1, 2, 2, 3, 5, 6 });
+        }
+
+        [Fact]
+        public void ItMergesSingleElementsArrays()
+        {
+            var nums1 = new[] { 2,0 };
+            int m = 1;
+            int n = 1;
+            var nums2 = new[] { 1 };
+            algorithm.Merge(nums1, m, nums2, n);
+
+            nums1.Should().BeEquivalentTo(new[] { 1, 2 });
+        }
+
+        [Fact]
+        public void ItMergesEmptySecondArray()
+        {
+            var nums1 = new[] { 1 };
+            int m = 1;
+            int n = 0;
+            var nums2 = Array.Empty<int>();
+            algorithm.Merge(nums1, m, nums2, n);
+
+            nums1.Should().BeEquivalentTo(new[] { 1 });
+        }
+    }
+}
