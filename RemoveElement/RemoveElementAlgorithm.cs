ecf8cc303489eeafa32257f4e1c59ee9212b780e
rrymar <rrymar@outlook.com>
2025-03-01 18:06:41 +0200

    https://leetcode.com/problems/remove-element/

---
+++ RemoveElement/RemoveElementAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.RemoveElement
+{
+    /// <summary>
+    /// https://leetcode.com/problems/remove-element/
+    /// </summary>
+    public class RemoveElementAlgorithm
+    {
+        public int RemoveElement(int[] nums, int val) {
+            var initialCount = nums.Length;
+            var output = new List<int>(initialCount);
+            for(int i = 0; i<nums.Length; i++)
+            {
+                if(nums[i] != val){
+                    output.Add(nums[i]);
+                }
+            } 
+
+            for(int j = 0; j<output.Count; j++){
+                nums[j] = output[j];
+            }
+
+            return output.Count;
+        }
+    }
+}
---
+++ RemoveElement/RemoveElementTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LeetCode.RemoveElement
+{
+    public class RemoveElementTests
+    {
+        private readonly RemoveElementAlgorithm algorithm = new();
+
+        [Fact]
+        public void ItRemovesElement()
+        {
+            var array = new[] {1, 2, 3, 4, 5};
+            var result = algorithm.RemoveElement(array, 2);
+
+            result.Should().Be(4);
+            array[0].Should().Be(1);
+            array[1].Should().Be(3);
+            array[2].Should().Be(4);
+            array[3].Should().Be(5);
+        }
+
+        [Fact]
+        public void ItRemovesMultipleElements()
+        {
+            var array = new[] {3, 3, 2, 3, 5};
+            var result = algorithm.RemoveElement(array, 3);
+
+            result.Should().Be(2);
+            array[0].Should().Be(2);
+            array[1].Should().Be(5);
+        }
+    }
+}
