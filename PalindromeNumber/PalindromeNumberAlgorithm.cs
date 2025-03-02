508857645782789b70078ff75aac6c18edbb513a
rrymar <rrymar@outlook.com>
2025-03-02 14:53:34 +0200

    https://leetcode.com/problems/palindrome-number/

---
+++ PalindromeNumber/PalindromeNumberAlgorithm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.PalindromeNumber
+{
+    /// <summary>
+    /// https://leetcode.com/problems/palindrome-number/
+    /// </summary>
+    public class PalindromeNumberAlgorithm
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+            if (x < 10) return true;
+
+            var nums = new List<int>();
+
+            while (x > 0)
+            {
+                var mod = x % 10;
+                nums.Add(mod);
+                x = x / 10;
+            }
+
+            for (int i = 0; i < nums.Count / 2; i++)
+            {
+                var rigthIndex = nums.Count - i - 1;
+                if (rigthIndex <= i) break;
+
+                var left = nums[i];
+                var rigth = nums[rigthIndex];
+                if (left != rigth) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PalindromeNumber/PalindromeNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using LeetCode.LongestSubstring;
+using Xunit;
+
+namespace LeetCode.PalindromeNumber
+{
+    public class PalindromeNumberTests
+    {
+        private readonly PalindromeNumberAlgorithm algorithm = new();
+
+        [Theory]
+        [InlineData(121,true)]
+        [InlineData(1,true)]
+        [InlineData(10,false)]
+        [InlineData(-121,false)]
+        public void IsReturnIsPalindrome(int number, bool result)
+        {
+            algorithm.IsPalindrome(number).Should().Be(result);
+        }
+
+    }
+}
